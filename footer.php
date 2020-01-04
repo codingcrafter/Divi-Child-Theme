5f6c0a07ecb74391766ecb6950848a5466f18e9f
Evan <marketing@evandrews.me>
2020-01-03 22:37:23 -0800

    Initial Upload of Files
    This is a bare bones divi child theme for use with divi builder. With slight modifications it may also be used with other themes.

---
+++ footer.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file adds the custom footer links to the Divi child theme.
+ * Author: Evan Drews https://evandrews.me
+ * @package Divi Parent Theme by Elegant Themes
+*/
+if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
+
+			<footer id="main-footer">
+				<?php get_sidebar( 'footer' ); ?>
+
+
+		<?php
+			if ( has_nav_menu( 'footer-menu' ) ) : ?>
+
+				<div id="et-footer-nav">
+					<div class="container">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'footer-menu',
+								'depth'          => '1',
+								'menu_class'     => 'bottom-nav',
+								'container'      => '',
+								'fallback_cb'    => '',
+							) );
+						?>
+					</div>
+				</div> <!-- #et-footer-nav -->
+
+			<?php endif; ?>
+
+				<div id="footer-bottom">
+					<div class="container clearfix">
+				<?php
+					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+						get_template_part( 'includes/social_icons', 'footer' );
+					}
+				?>
+
+						<p id="footer-info">Copyright &copy; <?php echo date("Y"); ?> <a href="#nogo">YOUR SITE</a> About your company.
+					</div>	<!-- .container -->
+					<div class="clear"></div>
+				</div>
+			</footer> <!-- #main-footer -->
+		</div> <!-- #et-main-area -->
+
+<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
+
+	</div> <!-- #page-container -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
